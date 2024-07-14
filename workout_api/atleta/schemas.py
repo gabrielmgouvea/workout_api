6508c7915efcbfecfe0a4d78dc75c671c31d2850
gabrielmgouvea <bielgouvea.rj@gmail.com>
2024-07-14 13:20:29 -0300

    Update schemas.py

--- workout_api/atleta/schemas.py
+++ workout_api/atleta/schemas.py
@@ -2,10 +2,7 @@ from typing import Annotated, Optional
 from pydantic import Field, PositiveFloat
 from workout_api.categorias.schemas import CategoriaIn
 from workout_api.centro_treinamento.schemas import CentroTreinamentoAtleta
-
 from workout_api.contrib.schemas import BaseSchema, OutMixin
-
-
 class Atleta(BaseSchema):
     nome: Annotated[str, Field(description='Nome do atleta', example='Joao', max_length=50)]
     cpf: Annotated[str, Field(description='CPF do atleta', example='12345678900', max_length=11)]
@@ -15,14 +12,15 @@ class Atleta(BaseSchema):
     sexo: Annotated[str, Field(description='Sexo do atleta', example='M', max_length=1)]
     categoria: Annotated[CategoriaIn, Field(description='Categoria do atleta')]
     centro_treinamento: Annotated[CentroTreinamentoAtleta, Field(description='Centro de treinamento do atleta')]
-
-
 class AtletaIn(Atleta):
     pass
-
-
 class AtletaOut(Atleta, OutMixin):
     pass
+
+class AtletaBasicOut(BaseSchema):
+    nome: Annotated[str, Field(description='Nome do atleta', example='Joao', max_length=50)]
+    categoria: Annotated[CategoriaIn, Field(description='Categoria do atleta')]
+    centro_treinamento: Annotated[CentroTreinamentoAtleta, Field(description='Centro de treinamento do atleta')]
 
 class AtletaUpdate(BaseSchema):
     nome: Annotated[Optional[str], Field(None, description='Nome do atleta', example='Joao', max_length=50)]
